09f825dd3a2d716f6d95bedd71ee67ead94c5e79
Hidetake Iwata <int128@gmail.com>
2021-12-06 11:15:05 +0900

    refactor: remove redundunt conditions (#229)

--- git-push-service/src/arrange.ts
+++ git-push-service/src/arrange.ts
@@ -1,5 +1,4 @@
 import { promises as fs } from 'fs'
-import * as path from 'path'
 import * as core from '@actions/core'
 import * as io from '@actions/io'
 import { Application, generateApplicationManifest } from './application'
@@ -15,47 +14,37 @@ type Inputs = {
   destinationRepository: string
 }
 
-export const arrangeManifests = async (inputs: Inputs): Promise<string[]> => {
-  await io.mkdirP(`${inputs.workspace}/applications`)
+export const arrangeManifests = async (inputs: Inputs): Promise<void> => {
+  await copyGeneratedManifest(inputs.manifests, `${inputs.workspace}/services/${inputs.service}`)
 
-  const services = new Set<string>()
-  for (const f of inputs.manifests) {
-    core.info(`add service ${inputs.service}`)
-    services.add(inputs.service)
-
-    const generatedManifestPath = `services/${inputs.service}`
-
-    await copyGeneratedManifest(f, `${inputs.workspace}/${generatedManifestPath}`)
-
-    // Always overwrite an application manifest.
-    // When a pull request is updated, an application manifest already exists and needs to be updated to the new sha.
-    await putApplicationManifest(
-      {
-        name: `${inputs.namespace}--${inputs.service}`,
-        project: inputs.project,
-        source: {
-          repository: inputs.destinationRepository,
-          branch: inputs.branch,
-          path: generatedManifestPath,
-        },
-        destination: {
-          namespace: inputs.namespace,
-        },
-        annotations: inputs.applicationAnnotations,
+  await putApplicationManifest(
+    {
+      name: `${inputs.namespace}--${inputs.service}`,
+      project: inputs.project,
+      source: {
+        repository: inputs.destinationRepository,
+        branch: inputs.branch,
+        path: `services/${inputs.service}`,
       },
-      inputs.workspace
-    )
-  }
-  return [...services]
+      destination: {
+        namespace: inputs.namespace,
+      },
+      annotations: inputs.applicationAnnotations,
+    },
+    inputs.workspace
+  )
 }
 
-const copyGeneratedManifest = async (source: string, destinationDir: string) => {
-  const destination = `${destinationDir}/${path.basename(source)}`
+const copyGeneratedManifest = async (manifests: string[], destinationDir: string) => {
   await io.mkdirP(destinationDir)
-  await io.cp(source, destination)
+  for (const f of manifests) {
+    core.info(`copying ${f} to ${destinationDir}`)
+    await io.cp(f, destinationDir)
+  }
 }
 
 const putApplicationManifest = async (application: Application, workspace: string) => {
+  await io.mkdirP(`${workspace}/applications`)
   const destination = `${workspace}/applications/${application.name}.yaml`
   core.info(`writing to ${destination}`)
   const content = generateApplicationManifest(application)
