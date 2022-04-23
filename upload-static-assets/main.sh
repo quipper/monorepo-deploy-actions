613c1b5ee90bc470af4dee64bf0f99dfe327146d
Hidetake Iwata <int128@gmail.com>
2022-04-23 12:53:45 +0900

    Add upload-static-assets

---
+++ upload-static-assets/main.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+set -o pipefail
+set -eux
+
+content_dir="$(mktemp -d)"
+container_id="$(docker create "$SOURCE_IMAGE")"
+docker cp "$container_id:$SOURCE_PATH" "$content_dir"
+docker rm "$container_id"
+
+docker run --rm amazon/aws-cli:2 --version
+docker run --rm \
+  -v /tmp:/tmp \
+  -e AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY \
+  -e AWS_SESSION_TOKEN \
+  amazon/aws-cli:2 \
+  s3 sync --no-progress "$content_dir" "$BUCKET_URI"
