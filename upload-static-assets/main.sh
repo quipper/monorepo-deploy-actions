25ec7139cd2d3a4b7326534c16cbe18a49d73562
Hidetake Iwata <int128@gmail.com>
2022-04-23 13:01:04 +0900

    Update main.sh

--- upload-static-assets/main.sh
+++ upload-static-assets/main.sh
@@ -7,11 +7,13 @@ container_id="$(docker create "$SOURCE_IMAGE")"
 docker cp "$container_id:$SOURCE_PATH" "$content_dir"
 docker rm "$container_id"
 
+find "$content_dir" -type f
+
 docker run --rm amazon/aws-cli:2 --version
 docker run --rm \
   -v /tmp:/tmp \
   -e AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY \
   -e AWS_SESSION_TOKEN \
-  amazon/aws-cli:2 \
+  amazon/aws-cli:latest \
   s3 sync --no-progress "$content_dir" "$BUCKET_URI"
