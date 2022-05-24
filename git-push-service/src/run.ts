faad18e1057821d682e6dad243c2fed4137b084a
Hidetake Iwata <int128@gmail.com>
2022-05-24 11:09:30 +0900

    Push service via pull request

--- git-push-service/src/run.ts
+++ git-push-service/src/run.ts
@@ -5,6 +5,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import * as git from './git'
 import * as glob from '@actions/glob'
+import { RequestError } from '@octokit/request-error'
 import { arrangeManifests } from './arrange'
 import { retry } from './retry'
 
@@ -75,14 +76,39 @@ const push = async (manifests: string[], inputs: Inputs): Promise<void | Error> 
     core.info('nothing to commit')
     return
   }
-  return await core.group(`push branch ${branch}`, async () => {
-    const message = `Deploy ${inputs.namespace}/${inputs.service}\n\n${commitMessageFooter}`
-    await git.commit(workspace, message)
-    const code = await git.pushByFastForward(workspace, branch)
-    if (code > 0) {
-      return new Error(`failed to push branch ${branch} by fast-forward`)
-    }
+  const message = `Deploy ${inputs.namespace}/${inputs.service}\n\n${commitMessageFooter}`
+  await git.commit(workspace, message)
+
+  // push the branch via a pull request
+  const topicBranch = `git-push-service--${inputs.namespace}--${inputs.service}--${Date.now()}`
+  const code = await core.group(`push branch ${topicBranch}`, () => git.pushByFastForward(workspace, topicBranch))
+  if (code > 0) {
+    return new Error(`failed to push branch ${topicBranch} by fast-forward`)
+  }
+
+  const octokit = github.getOctokit(inputs.token)
+  core.info(`creating a pull request from ${topicBranch} into ${branch}`)
+  const { data: pull } = await octokit.rest.pulls.create({
+    owner,
+    repo,
+    base: branch,
+    head: topicBranch,
   })
+  core.info(`created ${pull.html_url}`)
+  try {
+    const { data: merge } = await octokit.rest.pulls.merge({
+      owner,
+      repo,
+      pull_number: pull.number,
+      merge_method: 'squash',
+    })
+    core.info(`merged ${pull.html_url} as ${merge.sha}`)
+  } catch (e) {
+    if (e instanceof RequestError && e.status === 422) {
+      return e // retry when merge was failed
+    }
+    throw e
+  }
 }
 
 const commitMessageFooter = [
