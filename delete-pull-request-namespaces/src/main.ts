677fe7d3349376010cd4e54052826c71dc9d6f5d
Hidetake Iwata <int128@gmail.com>
2023-09-21 11:14:02 +0900

    Add delete-pull-request-namespaces action (#1126)
    * Add delete-pull-request-namespaces action
    
    * Fix ref does not exist
    
    * Refactor
    
    * Add namespace branch in e2e-test
    
    * Fix git-log
    
    * Refactor
    
    * Improve doc
    
    * Validate exclude-updated-within-minutes
    
    * Rename to applications.ts

---
+++ delete-pull-request-namespaces/src/main.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core'
+import { run } from './run'
+
+const main = async (): Promise<void> => {
+  await run({
+    overlay: core.getInput('overlay', { required: true }),
+    namespacePrefix: core.getInput('namespace-prefix', { required: true }),
+    sourceRepository: core.getInput('source-repository', { required: true }),
+    sourceRepositoryToken: core.getInput('source-repository-token', { required: true }),
+    destinationRepository: core.getInput('destination-repository', { required: true }),
+    destinationBranch: core.getInput('destination-branch', { required: true }),
+    destinationRepositoryToken: core.getInput('destination-repository-token', { required: true }),
+    excludeLabel: core.getInput('exclude-pull-request-label') || undefined,
+    excludeUpdatedWithinMinutes: getIntegerInput('exclude-updated-within-minutes'),
+    removeLabelOnDeletion: core.getInput('remove-label-on-deletion') || undefined,
+    commentOnDeletion: core.getInput('comment-on-deletion') || undefined,
+    dryRun: core.getBooleanInput('dry-run', { required: true }),
+  })
+}
+
+const getIntegerInput = (key: string) => {
+  const n = Number.parseInt(core.getInput(key, { required: true }))
+  if (Number.isSafeInteger(n)) {
+    return n
+  }
+  throw new Error(`Input ${key} must be an integer (${n})`)
+}
+
+main().catch((e: Error) => {
+  core.setFailed(e)
+  console.error(e)
+})
