5269eee9a274b9e10a4339e7c03d9668444611d9
Hidetake Iwata <int128@gmail.com>
2024-02-06 09:58:11 +0900

    Fix

--- open-backport-pull-request/src/main.ts
+++ open-backport-pull-request/src/main.ts
@@ -4,6 +4,7 @@ import { run } from './run'
 const main = async (): Promise<void> => {
   await run({
     githubToken: core.getInput('github-token', { required: true }),
+    headBranch: core.getInput('head-branch', { required: true }),
     baseBranch: core.getInput('base-branch', { required: true }),
   })
 }
