3a4a48843bfa4f261db6afe96c60dcb5ec50119e
kyontan <kyontan@monora.me>
2024-02-28 08:56:51 +0900

    feat(git-push-services-patch) Add services option to specify services (#1342)
    * feat(git-push-services-patch) Add services option to specify services
    
    * Generated by GitHub Actions (autofix / autofix)
    
    https://github.com/quipper/monorepo-deploy-actions/actions/runs/8068412499
    
    ---------
    
    Co-authored-by: update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>

--- git-push-services-patch/src/patch.ts
+++ git-push-services-patch/src/patch.ts
@@ -6,20 +6,21 @@ import * as path from 'path'
 type Inputs = {
   workspace: string
   patch: string
+  services: Set<string>
   excludeServices: Set<string>
 }
 
-export const addToServices = async (inputs: Inputs) => {
+export const addToServices = async (inputs: Inputs): Promise<void> => {
   const services = (await readdirOrEmpty(`${inputs.workspace}/services`))
     .filter((e) => e.isDirectory())
     .map((e) => e.name)
   core.info(`found ${services.length} service(s)`)
 
   for (const service of services) {
-    if (inputs.excludeServices.has(service)) {
-      core.info(`excluded service ${service}`)
+    if (shouldSkipService(service, inputs.services, inputs.excludeServices)) {
       continue
     }
+
     const serviceDirectory = `${inputs.workspace}/services/${service}`
     core.info(`copying the patch into ${serviceDirectory}`)
     await io.cp(inputs.patch, serviceDirectory, { force: true })
@@ -35,14 +36,28 @@ export const deleteFromServices = async (inputs: Inputs) => {
   core.info(`found ${services.length} service(s)`)
 
   for (const service of services) {
-    if (inputs.excludeServices.has(service)) {
-      core.info(`excluded service ${service}`)
+    if (shouldSkipService(service, inputs.services, inputs.excludeServices)) {
       continue
     }
+
     const patchPath = `${inputs.workspace}/services/${service}/${patchBasename}`
     core.info(`removing ${patchPath}`)
     await io.rmRF(patchPath)
   }
+}
+
+const shouldSkipService = (service: string, services: Set<string>, excludeServices: Set<string>) => {
+  if (services.size > 0 && !services.has(service)) {
+    core.info(`skipping ${service} because it is not specified`)
+    return true
+  }
+
+  if (excludeServices.has(service)) {
+    core.info(`excluded service ${service}`)
+    return true
+  }
+
+  return false
 }
 
 const readdirOrEmpty = async (dir: string) => {
