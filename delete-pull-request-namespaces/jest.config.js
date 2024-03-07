781ac5bdce32df5e2d4bda4cc00b5e07854f2b31
kyontan <kyontan@monora.me>
2024-03-08 01:37:53 +0900

    chore(delete-pull-request-namespaces) Fix jest issue

--- delete-pull-request-namespaces/jest.config.js
+++ delete-pull-request-namespaces/jest.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
+  // preset: 'ts-jest/presets/js-with-babel-esm',
+  // https://github.com/kulshekhar/ts-jest/issues/1057#issuecomment-1068342692
+  moduleNameMapper: {
+    '(.+)\\.js': '$1',
+  },
   clearMocks: true,
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
