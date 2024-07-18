d0b0b4f2c38430081d47f49e1fc6ece6ed4af7ce
Hidetake Iwata <int128@gmail.com>
2024-07-18 13:54:07 +0900

    Show debug log for AWSSecret

--- resolve-aws-secret-version/src/resolve.ts
+++ resolve-aws-secret-version/src/resolve.ts
@@ -49,6 +49,7 @@ const findAWSSecrets = (manifest: string): AWSSecret[] => {
     if (d.kind !== 'AWSSecret') {
       continue
     }
+    core.info(`Parsing the AWSSecret:\n${JSON.stringify(d, undefined, 2)}`)
     assertKubernetesAWSSecret(d)
 
     const name = d.metadata.name
