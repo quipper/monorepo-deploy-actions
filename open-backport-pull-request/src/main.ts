eb379d1527862d60c7676662f573dccb52f2b098
Hidetake Iwata <int128@gmail.com>
2024-10-03 17:21:45 +0900

    Support backport PR title and body (#1616)
    * Support backport PR title and body
    
    * doc

--- open-backport-pull-request/src/main.ts
+++ open-backport-pull-request/src/main.ts
@@ -1,14 +1,18 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  const outputs = await run({
+  const inputs = {
     githubToken: core.getInput('github-token', { required: true }),
     headBranch: core.getInput('head-branch', { required: true }),
     baseBranch: core.getInput('base-branch', { required: true }),
     skipCI: core.getBooleanInput('skip-ci', { required: true }),
     mergePullRequest: core.getBooleanInput('merge-pull-request', { required: true }),
-  })
+    pullRequestBody: core.getInput('pull-request-body', { required: false }),
+    pullRequestTitle: core.getInput('pull-request-title', { required: false }),
+  }
+  const outputs = await run(inputs, github.context)
   if (outputs) {
     core.setOutput('pull-request-url', outputs.pullRequestUrl)
     core.setOutput('base-branch', outputs.baseBranch)
---
+++ open-backport-pull-request/tests/format.test.ts
@@ -0,0 +1,62 @@
+import { Context } from '../src/github'
+import { getCommitMessage, getPullRequestBody, getPullRequestTitle } from '../src/format'
+
+const context: Context = {
+  actor: 'octocat',
+  repo: {
+    owner: 'owner',
+    repo: 'repo',
+  },
+  runId: 1,
+}
+
+describe('getCommitMessage', () => {
+  it('should return the commit message', () => {
+    const params = {
+      headBranch: 'production',
+      baseBranch: 'main',
+      skipCI: false,
+    }
+    expect(getCommitMessage(params, context)).toEqual(`Backport from production into main
+
+https://github.com/owner/repo/actions/runs/1`)
+  })
+
+  it('should return the commit message with skip ci', () => {
+    const params = {
+      headBranch: 'production',
+      baseBranch: 'main',
+      skipCI: true,
+    }
+    expect(getCommitMessage(params, context)).toEqual(`Backport from production into main [skip ci]
+
+https://github.com/owner/repo/actions/runs/1`)
+  })
+})
+
+describe('getPullRequestTitle', () => {
+  it('should return the pull request title', () => {
+    const params = {
+      headBranch: 'production',
+      baseBranch: 'main',
+      pullRequestTitle: 'Backport from HEAD_BRANCH into BASE_BRANCH',
+      pullRequestBody: '',
+    }
+    expect(getPullRequestTitle(params)).toEqual('Backport from production into main')
+  })
+})
+
+describe('getPullRequestBody', () => {
+  it('should return the pull request body', () => {
+    const params = {
+      headBranch: 'production',
+      baseBranch: 'main',
+      pullRequestTitle: '',
+      pullRequestBody: 'This is a backport pull request from HEAD_BRANCH into BASE_BRANCH',
+    }
+    expect(getPullRequestBody(params, context)).toEqual(`This is a backport pull request from production into main
+
+----
+https://github.com/owner/repo/actions/runs/1`)
+  })
+})
