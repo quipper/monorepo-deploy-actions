f5bd0027f443e7e9835fb81a5ffde717c563d966
Hidetake Iwata <int128@gmail.com>
2024-11-21 21:08:11 +0900

    Use msw for GitHub API (create-deploy-pull-request)

---
+++ create-deploy-pull-request/tests/github.ts
@@ -0,0 +1,67 @@
+import * as github from '@actions/github'
+import { setupServer } from 'msw/node'
+import { http, HttpResponse } from 'msw'
+
+export const server = setupServer(
+  http.get('https://api.github.com/repos/test-owner/test-repo-1/branches/release', () =>
+    HttpResponse.json({
+      commit: {
+        sha: 'commit-sha-1-release',
+      },
+    }),
+  ),
+  http.get(
+    'https://api.github.com/repos/test-owner/test-repo-1/branches/production',
+    () => new HttpResponse(null, { status: 404 }),
+  ),
+  http.post('https://api.github.com/repos/test-owner/test-repo-1/git/refs', async ({ request }) => {
+    expect(JSON.parse(await request.text())).toStrictEqual({
+      ref: 'refs/heads/production',
+      sha: 'commit-sha-1-release',
+    })
+    new HttpResponse(null, { status: 201 })
+  }),
+
+  http.get('https://api.github.com/repos/test-owner/test-repo-2/branches/release', () =>
+    HttpResponse.json({
+      commit: {
+        sha: 'commit-sha-2-release',
+      },
+    }),
+  ),
+  http.get('https://api.github.com/repos/test-owner/test-repo-2/branches/production', () =>
+    HttpResponse.json({
+      // Omit an example response
+    }),
+  ),
+  http.get('https://api.github.com/repos/test-owner/test-repo-2/pulls', ({ request }) => {
+    const url = new URL(request.url)
+    expect(url.searchParams.get('base')).toBe('production')
+    expect(url.searchParams.get('head')).toBe('test-owner:release')
+    return HttpResponse.json([])
+  }),
+  http.post('https://api.github.com/repos/test-owner/test-repo-2/pulls', async ({ request }) => {
+    expect(JSON.parse(await request.text())).toStrictEqual({
+      base: 'production',
+      head: 'release',
+      title: 'Deploy service to production at 2023-09-07 15:01:02',
+      body: 'Hello',
+      draft: true,
+    })
+    return HttpResponse.json({
+      html_url: 'https://github.com/test-owner/test-repo-2/pulls/100',
+    })
+  }),
+  http.post('https://api.github.com/repos/test-owner/test-repo-2/pulls//requested_reviewers', () =>
+    HttpResponse.json({
+      // Omit an example response
+    }),
+  ),
+  http.post('https://api.github.com/repos/test-owner/test-repo-2/issues//assignees', () =>
+    HttpResponse.json({
+      // Omit an example response
+    }),
+  ),
+)
+
+export const getOctokit = () => github.getOctokit('GITHUB_TOKEN', { request: { fetch } })
--- create-deploy-pull-request/tests/run.test.ts
+++ create-deploy-pull-request/tests/run.test.ts
@@ -1,113 +1,44 @@
 import { run } from '../src/run.js'
-import { RequestError } from '@octokit/request-error'
+import { getOctokit, server } from './github.js'
 
-const octokitMock = {
-  rest: {
-    repos: {
-      getBranch: jest.fn(),
-    },
-    pulls: {
-      list: jest.fn(),
-      create: jest.fn(),
-      requestReviewers: jest.fn(),
-    },
-    issues: {
-      addAssignees: jest.fn(),
-      addLabels: jest.fn(),
-    },
-    git: {
-      createRef: jest.fn(),
-      updateRef: jest.fn(),
-    },
-  },
-}
-
-jest.mock('@actions/github', () => ({ getOctokit: () => octokitMock }))
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 it('should create base branch if not exist', async () => {
-  octokitMock.rest.repos.getBranch.mockRejectedValueOnce(
-    new RequestError('not found', 404, {
-      request: { method: 'GET', url: 'dummy', headers: {} },
-      response: { status: 404, url: 'dummy', headers: {}, data: {} },
-    }),
+  await run(
+    {
+      head: 'release',
+      base: 'production',
+      title: 'Deploy service to production',
+      body: 'Hello',
+      actor: 'octocat',
+      labels: [],
+      draft: true,
+      owner: 'test-owner',
+      repo: 'test-repo-1',
+      now: () => new Date(Date.UTC(2023, 8, 7, 6, 1, 2, 0)),
+      timeZone: 'Asia/Tokyo',
+    },
+    getOctokit(),
   )
-  octokitMock.rest.repos.getBranch.mockResolvedValueOnce({
-    data: { commit: { sha: 'release-sha' } },
-  })
-
-  await run({
-    head: 'release',
-    base: 'production',
-    title: 'Deploy service to production',
-    body: 'Hello',
-    actor: 'octocat',
-    labels: [],
-    draft: true,
-    owner: 'OWNER',
-    repo: 'REPO',
-    now: () => new Date(Date.UTC(2023, 8, 7, 6, 1, 2, 0)),
-    timeZone: 'Asia/Tokyo',
-    token: 'GITHUB_TOKEN',
-  })
-
-  expect(octokitMock.rest.repos.getBranch).toHaveBeenCalledTimes(2)
-  expect(octokitMock.rest.repos.getBranch).toHaveBeenNthCalledWith(1, {
-    owner: 'OWNER',
-    repo: 'REPO',
-    branch: 'production',
-  })
-  expect(octokitMock.rest.repos.getBranch).toHaveBeenNthCalledWith(2, {
-    owner: 'OWNER',
-    repo: 'REPO',
-    branch: 'release',
-  })
-  expect(octokitMock.rest.git.createRef).toHaveBeenCalledWith({
-    owner: 'OWNER',
-    repo: 'REPO',
-    ref: 'refs/heads/production',
-    sha: 'release-sha',
-  })
 })
 
 it('should create pull request if base branch exists', async () => {
-  octokitMock.rest.repos.getBranch.mockResolvedValueOnce({
-    data: { commit: { sha: 'production-exists' } },
-  })
-  octokitMock.rest.pulls.list.mockResolvedValueOnce({
-    data: [],
-  })
-  octokitMock.rest.pulls.create.mockResolvedValueOnce({
-    data: { html_url: 'https://github.com/OWNER/REPO/pulls/1' },
-  })
-
-  await run({
-    head: 'release',
-    base: 'production',
-    title: 'Deploy service to production',
-    body: 'Hello',
-    actor: 'octocat',
-    labels: [],
-    draft: true,
-    owner: 'OWNER',
-    repo: 'REPO',
-    now: () => new Date(Date.UTC(2023, 8, 7, 6, 1, 2, 0)),
-    timeZone: 'Asia/Tokyo', // UTC+9
-    token: 'GITHUB_TOKEN',
-  })
-
-  expect(octokitMock.rest.repos.getBranch).toHaveBeenCalledTimes(1)
-  expect(octokitMock.rest.repos.getBranch).toHaveBeenNthCalledWith(1, {
-    owner: 'OWNER',
-    repo: 'REPO',
-    branch: 'production',
-  })
-  expect(octokitMock.rest.pulls.create).toHaveBeenCalledWith({
-    owner: 'OWNER',
-    repo: 'REPO',
-    head: 'release',
-    base: 'production',
-    title: 'Deploy service to production at 2023-09-07 15:01:02',
-    body: 'Hello',
-    draft: true,
-  })
+  await run(
+    {
+      head: 'release',
+      base: 'production',
+      title: 'Deploy service to production',
+      body: 'Hello',
+      actor: 'octocat',
+      labels: [],
+      draft: true,
+      owner: 'test-owner',
+      repo: 'test-repo-2',
+      now: () => new Date(Date.UTC(2023, 8, 7, 6, 1, 2, 0)),
+      timeZone: 'Asia/Tokyo', // UTC+9
+    },
+    getOctokit(),
+  )
 })
