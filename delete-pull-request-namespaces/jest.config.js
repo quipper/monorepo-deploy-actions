5dfa6167e6fea80d8da65a56f9a295f4ad9228cd
Hidetake Iwata <int128@gmail.com>
2024-11-21 14:30:53 +0900

    Align jest config (#1716)

--- delete-pull-request-namespaces/jest.config.js
+++ delete-pull-request-namespaces/jest.config.js
@@ -1,12 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
-  // preset: 'ts-jest/presets/js-with-babel-esm',
-  // https://github.com/kulshekhar/ts-jest/issues/1057#issuecomment-1068342692
-  moduleNameMapper: {
-    '(.+)\\.js': '$1',
-  },
+  preset: 'ts-jest/presets/default-esm',
   clearMocks: true,
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  verbose: true,
+  // https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 }
