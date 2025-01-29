49d433bb3c81e211450926f4707495a13595ae9f
kyontan <kyontan@monora.me>
2025-01-30 02:04:18 +0900

    feat(get-service-versions) Initial implementation

---
+++ get-service-versions/src/main.ts
@@ -0,0 +1,17 @@
+import * as core from '@actions/core'
+import { run } from './run.js'
+
+const main = async (): Promise<void> => {
+  await run({
+    overlay: core.getInput('overlay', { required: true }),
+    namespace: core.getInput('namespace', { required: true }),
+    sourceRepository: core.getInput('source-repository', { required: true }),
+    destinationRepository: core.getInput('destination-repository', { required: true }),
+    destinationRepositoryToken: core.getInput('destination-repository-token', { required: true }),
+  })
+}
+
+main().catch((e: Error) => {
+  core.setFailed(e)
+  console.error(e)
+})
