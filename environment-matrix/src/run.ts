011677ed3b479eeca2d29bfc457efc09949b0784
Hidetake Iwata <int128@gmail.com>
2025-03-07 17:49:41 +0900

    Fix assignment of github-deployment-url

--- environment-matrix/src/run.ts
+++ environment-matrix/src/run.ts
@@ -14,17 +14,13 @@ type Outputs = {
   json: rule.Outputs[]
 }
 
-type Environment = rule.Environment & {
-  'github-deployment-url'?: string
-}
-
 export const run = async (inputs: Inputs): Promise<Outputs> => {
   const rules = rule.parseRulesYAML(inputs.rules)
   core.startGroup('Rules')
   core.info(JSON.stringify(rules, undefined, 2))
   core.endGroup()
 
-  const environments: Environment[] | undefined = matcher.find(github.context, rules)
+  const environments = matcher.find(github.context, rules)
   if (environments === undefined) {
     throw new Error(`no environment to deploy`)
   }
@@ -34,7 +30,7 @@ export const run = async (inputs: Inputs): Promise<Outputs> => {
   for (const environment of environments) {
     if (environment['github-deployment']) {
       const deployment = await createDeployment(octokit, github.context, environment['github-deployment'])
-      environment['github-deployment-url'] = deployment.url
+      environment.outputs['github-deployment-url'] = deployment.url
     }
   }
 
