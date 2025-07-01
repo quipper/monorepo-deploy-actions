f136ff30aff5238a6d7f95bec278fd4bb64beecb
Hidetake Iwata <int128@gmail.com>
2025-07-01 10:48:40 +0900

    Refactor

--- bootstrap-pull-request/tests/prebuilt.test.ts
+++ bootstrap-pull-request/tests/prebuilt.test.ts
@@ -55,14 +55,14 @@ describe('syncServicesFromPrebuilt', () => {
     expect(await readContent(`${namespaceDirectory}/services/b/generated.yaml`)).toBe(serviceB)
   })
 
-  it('does not write a service if in the changedServices', async () => {
+  it('does not overwrite the changed services', async () => {
     const namespaceDirectory = await createEmptyDirectory()
     await fs.mkdir(`${namespaceDirectory}/applications`)
     await fs.mkdir(`${namespaceDirectory}/services/a`, { recursive: true })
-    await fs.writeFile(`${namespaceDirectory}/applications/pr-123--a.yaml`, applicationPushedOnOutdatedCommit)
+    await fs.writeFile(`${namespaceDirectory}/applications/pr-123--a.yaml`, existingApplicationA)
     await fs.writeFile(`${namespaceDirectory}/services/a/generated.yaml`, 'this-should-be-kept')
 
-    await syncServicesFromPrebuilt({
+    const services = await syncServicesFromPrebuilt({
       applicationContext: {
         overlay: 'pr',
         namespace: 'pr-123',
@@ -78,17 +78,36 @@ describe('syncServicesFromPrebuilt', () => {
       substituteVariables: new Map<string, string>([['NAMESPACE', 'pr-123']]),
     })
 
+    expect(services).toStrictEqual<Service[]>([
+      {
+        service: 'a',
+        builtFrom: {
+          pullRequest: {
+            headRef: 'topic-branch',
+            headSha: 'topic-sha',
+          },
+        },
+      },
+      {
+        service: 'b',
+        builtFrom: {
+          prebuilt: {
+            prebuiltBranch: 'prebuilt/source-repository/pr',
+            builtFrom: { headRef: 'main', headSha: 'main-branch-sha' },
+          },
+        },
+      },
+    ])
+
     expect(await fs.readdir(`${namespaceDirectory}/applications`)).toStrictEqual(['pr-123--a.yaml', 'pr-123--b.yaml'])
-    expect(await readContent(`${namespaceDirectory}/applications/pr-123--a.yaml`)).toBe(
-      applicationPushedOnOutdatedCommit,
-    )
+    expect(await readContent(`${namespaceDirectory}/applications/pr-123--a.yaml`)).toBe(existingApplicationA)
     expect(await readContent(`${namespaceDirectory}/services/a/generated.yaml`)).toBe('this-should-be-kept')
   })
 
   it('deletes a service which does not exist in the prebuilt branch', async () => {
     const namespaceDirectory = await createEmptyDirectory()
     await fs.mkdir(`${namespaceDirectory}/applications`)
-    await fs.writeFile(`${namespaceDirectory}/applications/pr-123--outdated.yaml`, applicationPushedOnOutdatedCommit)
+    await fs.writeFile(`${namespaceDirectory}/applications/pr-123--outdated.yaml`, `this-should-be-deleted`)
 
     const services = await syncServicesFromPrebuilt({
       applicationContext: {
@@ -217,7 +236,7 @@ spec:
       prune: true
 `
 
-const applicationPushedOnOutdatedCommit = `\
+const existingApplicationA = `\
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata:
@@ -228,7 +247,7 @@ metadata:
   annotations:
     github.action: git-push-service
     github.head-ref: topic-branch
-    github.head-sha: outdated-sha
+    github.head-sha: topic-sha
 spec:
   project: source-repository
   source:
