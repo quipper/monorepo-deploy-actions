1461dd7160223229ec5fc9bb4164bdd7e1a29648
Hidetake Iwata <int128@gmail.com>
2025-11-26 12:49:46 +0900

    Migrate to Biome (template)

--- template/tests/run.test.ts
+++ template/tests/run.test.ts
@@ -1,5 +1,5 @@
+import { expect, test } from 'vitest'
 import { run } from '../src/run.js'
-import { test, expect } from 'vitest'
 
 test('run successfully', async () => {
   await expect(run({ name: 'foo' })).resolves.toBeUndefined()
